1d079d3e417c8a593687b135d76576220c581d39
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2020-11-04 08:11:28 -0500

    Update App.js

--- navBar/src/App.js
+++ navBar/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Home from './pages';
 
-function App() 
+function App() {
   return (
     <Router>
       <Home />
