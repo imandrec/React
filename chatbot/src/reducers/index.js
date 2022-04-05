4372c1812048781828d24a7ec7ce2eaa31321ea7
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-04-05 08:08:46 -0500

    Delete chatbot directory

--- chatbot/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// import dependencies
-import { combineReducers } from "redux";
-
-// Import reducers
-import paras from "./paras"
-
-// export combined reducers
-export default combineReducers({paras})
